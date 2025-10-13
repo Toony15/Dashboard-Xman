6ad6a5e205615205cff4e5987e70133240109d3d
nauvalgymnasti7 <nauvalgymnasti7@gmail.com>
2025-10-14 01:31:22 +0700

    .

--- app.py
+++ app.py
@@ -53,14 +53,38 @@ with st.container(horizontal=True, vertical_alignment="bottom", gap="small", hor
     st.image(logo1, width=60)
     st.title("Expert Calculator", width="content")
     
-st.markdown(
-    """
-    Selamat datang di **ExmanComp Dashboard** 👋  
-    Pilih halaman di sidebar untuk mulai menavigasi:
-    - Learning Impact 1  
-    - Learning Hour  
-    - Variation  
-    - Compensation  
-    - Data Manager  
-    """
-)
+st.text("")
+st.text("")
+st.text("Expert Calculator adalah platform perhitungan digital yang dirancang untuk mendukung proses evaluasi dan pemberian kompensasi bagi para expert di lingkungan organisasi. Aplikasi ini mengolah data dari berbagai aspek seperti Learning Hours, Learning Impact, dan Variation untuk menghasilkan skor akhir yang mencerminkan kinerja serta kontribusi setiap individu secara objektif. Dengan sistem perhitungan otomatis yang mengacu pada pedoman Expert Management 2025, Expert Calculator membantu memastikan proses evaluasi berlangsung lebih transparan, efisien, dan adil. Melalui pendekatan berbasis data, platform ini juga mendukung pengambilan keputusan yang lebih akurat dalam pengelolaan reward dan pengembangan level expert di masa mendatang.")
+
+st.text("")
+st.text("")
+with st.container(horizontal=True, gap="small"):
+    
+    with st.container(border=True, height=300):
+        with st.container(horizontal=True):
+            lh = 'assets/lh.png'
+            st.image(lh, width=60)
+            st.subheader("Learning Hours")
+        st.text("Total realisasi jam kerja pembelajaran yang digunakan dalam penugasan Knowledge Dissemination (mengajar, content development, coaching, speaker, dsb.)")
+
+    with st.container(border=True, height=300):
+        with st.container(horizontal=True):
+            imp = 'assets/imp.png'
+            st.image(imp, width=60)
+            st.subheader("Learning Impact")
+        st.text("Hasil pengukuran efektivitas program pembelajaran yang mencerminkan sejauh mana kegiatan pembelajaran memberikan dampak positif. (LIM 1–Reaksi Peserta, LIM 2-Peningkatan Knowledge/Skill, LIM 3-Perubahan Perilaku, etc.)")
+    with st.container(border=True, height=300):
+            with st.container(horizontal=True):
+                var = 'assets/var.png'
+                st.image(var, width=60)
+                st.subheader("Variasi Penugasan")
+            st.text("Keragaman jenis penugasan yang dijalankan expert pada periode program Knowledge Dissemination.")
+
+
+
+
+
+
+
+
