5e82e7f2fe7e536ce559d283a7eede155eed1e25
nauvalgymnasti7 <nauvalgymnasti7@gmail.com>
2025-10-27 10:36:24 +0700

    start develop compensation page

--- modules/compensation.py
+++ modules/compensation.py
@@ -1,2 +1,9 @@
+import streamlit as st
+import pandas as pd
+import io
+from dbConfig import get_db_connection
+from google import genai
+from dataManager import load_all_data
+
 def compensation_page():
-    print("halaman compensation")
+    st.title("Compensation")
